2a86657a4c55bdb087317cf9db81c464464c823e
JP Flouret <jpflouret@gmail.com>
2021-08-09 09:18:08 -0700

    Prepared package for publish to PyPI (#6)

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from setuptools import find_packages, setup
 
-setup(name="iqtrade", packages=find_packages())
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="iqtrade",
+    version="0.0.1",
+    author="JP Flouret",
+    author_email="jpflouret@gmail.com",
+    description="Python wrapper around Questrade IQ API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jpflouret/iqtrade",
+    packages=find_packages(),
+    install_requires=["requests"],
+    python_requires=">=3.9",
+)
